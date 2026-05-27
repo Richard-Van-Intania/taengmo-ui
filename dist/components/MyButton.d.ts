63a3de4ed85ccc6b79e53931fda099739fa705b8
Ittipat Pattum <nampu@finema.co>
2026-05-27 17:46:08 +0700

    save

---
+++ dist/components/MyButton.d.ts
@@ -0,0 +1,4 @@
+import type { ButtonHTMLAttributes } from "react";
+type Props = ButtonHTMLAttributes<HTMLButtonElement>;
+export declare function MyButton(props: Props): import("react/jsx-runtime").JSX.Element;
+export {};
