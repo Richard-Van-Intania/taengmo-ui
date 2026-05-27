95a17b29d7f5e9d0a91e55c8f82f4eb6cc7468b2
Ittipat Pattum <nampu@finema.co>
2026-05-27 18:49:12 +0700

    save

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,2 @@
-import { jsx as e } from "react/jsx-runtime";
-//#region src/components/MyButton.tsx
-function t(t) {
-	return /* @__PURE__ */ e("button", {
-		...t,
-		className: "\n        bg-primary-dev\n        hover:bg-(--color-primary-hover)\n        text-black\n        px-4\n        py-2\n        rounded-(--radius-button)\n        transition-colors\n      ",
-		children: "BT"
-	});
-}
-//#endregion
-export { t as MyButton };
+import "./index.css";
+export * from "./components/MyButton";
