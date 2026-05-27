5e15eff355f42bb1ca893799a65442722e5691ea
Ittipat Pattum <nampu@finema.co>
2026-05-27 19:08:27 +0700

    save

--- dist/components/MyButton.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-export function MyButton(props) {
-    return (_jsx("button", { ...props, className: "\n        bg-primary-dev\n        hover:bg-(--color-primary-hover)\n        text-black\n        px-4\n        py-2\n        rounded-(--radius-button)\n        transition-colors\n      ", children: "BT" }));
-}
