b56c8c1c95a14c88101b1489e30b92979a2f90c3
Ittipat Pattum <nampu@finema.co>
2026-05-28 15:44:57 +0700

    save

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import dts from "vite-plugin-dts";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "TAENGMO-UI",
+
+      name: "TaengmoUI",
+
       fileName: "index",
+
       formats: ["es"],
     },
+
+    cssCodeSplit: false,
+
     rollupOptions: {
       external: ["react", "react-dom", "react/jsx-runtime"],
     },
-    cssCodeSplit: false,
   },
 });
